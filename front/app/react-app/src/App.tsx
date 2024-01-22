aea8e8887c52e8e2480cfe8c732858cc2bf0718f
kuroishihiromu <kuroishihiromu@gmail.com>
2024-01-23 02:57:08 +0900

    Routing

--- front/app/react-app/src/App.tsx
+++ front/app/react-app/src/App.tsx
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="border border-gray-400 rounded-2xl p-2 m-2 flex justify-around items-center">
-      <h1 className="text-3xl font-bold underline">
-        Hello Tailwind CSS!
-      </h1>
-      <p className="m-0 text-gray-400">Tailwind CSSです</p>
-      <button className="bg-gray-300 border-0 p-2 rounded-md hover:bg-gray-400 hover:text-white">ボタン</button>
+    <div className="App">
+      <Link to="/">HOME</Link>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
