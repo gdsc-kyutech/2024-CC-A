c6343dc06d91ccc2fea4f75ca42deccd2301ba87
kuroishihiromu <kuroishihiromu@gmail.com>
2024-01-23 10:56:11 +0900

    POSTメソッドの追加

--- front/app/react-app/src/components/ImageTransfer.jsx
+++ front/app/react-app/src/components/ImageTransfer.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios  from 'axios';
 
 const ImageTransfer = () => {
     const [image, setImage] = useState(null);
@@ -21,6 +22,15 @@ const ImageTransfer = () => {
     const handleImageUpload = () => {
         // POSTリクエストを送信する処理を実装する
         // ここで`image`の値を使用してBase64形式の画像データを送信する
+        console.log(image);
+        axios.post('http://localhost:5000/analyze_img', {
+            content: image
+        }).then((response) => {
+            console.log(response.data);
+        }).catch((error) => {
+            console.log(error);
+        });
+
     };
 
     return (
