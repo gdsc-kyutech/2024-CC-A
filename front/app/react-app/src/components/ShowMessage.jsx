8442ad41e7fa41b199b8792e941bba0f9de66ab0
kuroishihiromu <kuroishihiromu@gmail.com>
2024-01-30 12:07:04 +0900

    ヘッダーコンポーネント作成

--- front/app/react-app/src/components/ShowMessage.jsx
+++ front/app/react-app/src/components/ShowMessage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ShowMessage = ({ message }) => {
     return (
         <div className='w-80'>
+            This is the hint.
             <p>{message}</p>
         </div>
     );
