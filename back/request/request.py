4bbcbba4a0e9195aaab14fed311f6272f4024ed3
mio256 <reiya@woorth.io>
2024-02-05 11:13:16 +0900

    :bug: back; request; data->json

--- back/request/request.py
+++ back/request/request.py
@@ -10,7 +10,7 @@ print(r.text)
 # カレントディレクトリにあるlight.jpegを読み込む
 with open('light.jpeg', 'rb') as f:
     img = f.read()
-d = {'image':base64.b64encode(img)}
+d = {'image': base64.b64encode(img).decode('utf-8')}
 pprint.pprint(d)
-r = requests.post(localhost+'/analyze_image', data=d)
+r = requests.post(localhost+'/analyze_image', json=d, headers={'Content-Type': 'application/json'})
 print(r.text)
